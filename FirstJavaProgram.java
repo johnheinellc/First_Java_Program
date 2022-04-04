29c5621978f385fa10f8a26078341ab415d547f6
johnheinellc <johnheinellc@gmail.com>
2022-04-04 14:41:27 -0400

    First Commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+	public static void main(String[] args) {
+		System.out.println("I am John Heine,");
+        System.out.println("I am 333 years old,");
+        System.out.println("I hail from Jacksonville,FL");
+	}
+}
